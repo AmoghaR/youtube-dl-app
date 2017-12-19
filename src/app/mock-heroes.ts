2c75bc1edd089cb7403bbe142c3a672af6113c57
Amogha Ram <srinee19@gmail.com>
2017-12-19 19:59:27 +0530

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+{id: 2, name: 'dhruva'},
+{id: 3, name: 'darshan'},
+{id: 4, name: 'kishore'},
+{id: 5, name: 'saroj'},
+{id: 6, name: 'kamesh'},
+{id: 7, name: 'sumesh'},
+];
